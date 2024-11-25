308cbe878bda956275b9c6385539b7c35122cedc
eavarady <esteban.varady@gmail.com>
2024-11-25 21:47:15 +0100

    Built iterator in message() to check every combination of tables

--- MatchFinder.java
+++ MatchFinder.java
@@ -1,6 +1,6 @@
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.List; // no additional java.sql imports allowed!
+import java.util.List;
 
 
 public class MatchFinder implements IMatchFinder {
@@ -20,9 +20,36 @@ public class MatchFinder implements IMatchFinder {
 	//Needs to loop through database until it finds an union or cartesian product, else return "NO MATCH"
 	//Should probably separate concerns and make separate functions to check for UNION and CC
 	//Needs to check every possible combination, without any duplicates or unions/cc of itself of course
+	@Override
 	public String message() throws SQLException {
-		//TO DO
-		return "";
+		//Table X Iterator
+        for (int i = 0; i < tables.size(); i++) {
+			//Table Y Iterator
+            for (int j = i + 1; j < tables.size(); j++) {
+				//Table Z Iterator
+                for (int k = 0; k < tables.size(); k++) {
+					//Assign table names to instance variables
+					relX = tables.get(i);
+                    relY = tables.get(j);
+					relZ = tables.get(k);
+
+					//Check that table Z is not the same as X or Y before starting to check
+                    if (relZ.equals(relX) == false && relZ.equals(relY) == false) {
+                        //Call  if cartesianCheck() == True and if unionCheck() == True
+						//Get number of rows and compare beforehand to filter out certain tables? Could improve performance
+                    }
+                }
+            }
+        }
+        return "NO MATCH";
+    }
+	
+	private boolean unionCheck(String table1, String table2, String z) throws SQLException {
+		return true;
+	}
+
+	private boolean cartesianCheck(String table1, String table2, String z) throws SQLException {
+		return true;
 	}
 
 	//Generates a CREATE TABLE query to copy over relation X/Y from input.db to output.db
